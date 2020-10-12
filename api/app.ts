3277fbd5cd47b7610cfbd00bcb58b2b40a72ca28
Matheus Leal <matheusaquino@id.uff.br>
2020-10-12 20:27:58 -0300

    using cors

--- api/app.ts
+++ api/app.ts
@@ -1,4 +1,5 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import router from './routes.ts';
 
 const HOST = '127.0.0.1';
@@ -6,6 +7,7 @@ const PORT = 7700;
 
 const app = new Application();
 
+app.use(oakCors())
 app.use(router.routes());
 app.use(router.allowedMethods());
 
